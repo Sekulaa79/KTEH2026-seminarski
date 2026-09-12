b5e565350e685d8b916dce5f1ddeac1df026c406
Aleksandar <alexovicaco@gmail.com>
2026-09-12 02:09:31 +0200

    dodata logo ikonica umesto dosadasnjeg teksta

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,26 @@
 // src/components/Navbar.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.jpg';
 import './Navbar.css';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
+
       <div className="logo-container">
-        
-        <Link to="/">Planina Logo</Link> 
+        <Link to="/">
+          <img src={logo} alt="Planina" style={{ height: '50px', objectFit: 'contain' }} />
+        </Link>
       </div>
+
       <div className="nav-links">
         <Link to="/">POCETNA</Link>
         <Link to="/putuj">PUTUJ</Link>
         <Link to="/moj-put">MOJ PUT</Link>
         <Link to="/login" className="register-btn">REGISTRACIJA</Link>
       </div>
+
     </nav>
   );
 };
