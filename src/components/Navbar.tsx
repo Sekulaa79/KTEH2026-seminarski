0c0173409a8507f3654cfe7c0bd532fc08c79937
Aleksandar <alexovicaco@gmail.com>
2026-09-13 03:50:58 +0200

    U NavBaru promenjen link za registraciju i dodato dugme za odjavu ako je korisnik ulogovan

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,46 @@
-// src/components/Navbar.tsx
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../images/logo.jpg';
+import './Navbar.css';
+import { AppContext } from '../context/AppContext';
+import { AuthService } from '../models/User';
+
+const authService = new AuthService();
+
+const Navbar: React.FC = () => {
+  const { korisnik, postaviKorisnika } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    authService.logout();
+    postaviKorisnika(null);
+    navigate('/');
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <Link to="/"><img src={logo} alt="Planina" style={{ height: '50px', objectFit: 'contain' }} /></Link> 
+      </div>
+      <div className="nav-links">
+        <Link to="/">POCETNA</Link>
+        <Link to="/putuj">PUTUJ</Link>
+        <Link to="/moj-put">MOJ PUT</Link>
+        {korisnik ? (
+          <button onClick={handleLogout} className="register-btn" style={{ background: 'transparent', cursor: 'pointer' }}>ODJAVI SE</button>
+        ) : (
+          <Link to="/registracija" className="register-btn">REGISTRACIJA</Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
+
+{/*
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.jpg';
@@ -26,3 +68,4 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+*/}
