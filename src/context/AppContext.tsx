8f129efc5a3f3fb32131a4607be7be9debf2ef95
Aleksandar <alexovicaco@gmail.com>
2026-09-13 03:29:44 +0200

    prosiren AppContext da prati ko je ulogovan

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,25 +1,37 @@
 import React, { createContext, useState, ReactNode } from 'react';
 import { IDestination, TripManager } from '../models/Destination';
+import { IUser, AuthService } from '../models/User';
 
 // Definišemo šta naš Context sadrži
 interface AppContextType {
   mojPut: IDestination[];
   dodajUPut: (destinacija: IDestination) => void;
+  korisnik: IUser | null;
+  postaviKorisnika: (user: IUser | null) => void;
 }
 
 // Inicijalizujemo Context
 export const AppContext = createContext<AppContextType>({
   mojPut: [],
-  dodajUPut: () => {}
+  dodajUPut: () => {},
+  korisnik: null,
+  postaviKorisnika: () => {}
 });
 
 // Instanciramo klasu
 const tripManager = new TripManager();
+const authService = new AuthService(); // Inicijalizujemo drugu klasu
 
 export const AppProvider: React.FC<{children: ReactNode}> = ({ children }) => {
   const [mojPut, setMojPut] = useState<IDestination[]>(tripManager.getTrips());
+  // Prilikom ucitavanja aplikacije, proveravamo da li u memoriji vec postoji korisnik
+  const [korisnik, setKorisnik] = useState<IUser | null>(authService.getCurrentUser());
 
   const dodajUPut = (destinacija: IDestination) => {
+    if (!korisnik) {
+      alert("Morate se prijaviti da biste dodali putovanje!");
+      return;
+    }
     // Provera da li destinacija sa ovim ID-jem već postoji u nizu
     const vecDodato = mojPut.some(dest => dest.id === destinacija.id);
 
@@ -33,8 +45,12 @@ export const AppProvider: React.FC<{children: ReactNode}> = ({ children }) => {
     alert(`Uspešno dodato: ${destinacija.naziv}`);
   };
 
+  const postaviKorisnika = (user: IUser | null) => {
+    setKorisnik(user);
+  };
+
   return (
-    <AppContext.Provider value={{ mojPut, dodajUPut }}>
+    <AppContext.Provider value={{ mojPut, dodajUPut, korisnik, postaviKorisnika }}>
       {children}
     </AppContext.Provider>
   );
